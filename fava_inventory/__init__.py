ad2bec62e66f94086ba08f1e4a549413c71390e0
ronaldcheng218 <ronaldcheng218@gmail.com>
2020-11-15 20:52:29 +1100

    1.0.0

--- fava_inventory/__init__.py
+++ fava_inventory/__init__.py
@@ -1,123 +1,82 @@
-"""Portfolio list extension for Fava.
-This is a simple example of Fava's extension reports system.
+"""
+Physical inventory list extension for fava.
+
+Usage: 
+YYYY-MM-DD custom "fava-extension" "fava_inventory" "[('Consignment', 'In-Transit', 'In-Stock'), 'inventory.beancount']"
 """
 import re
 
-from beancount.core.data import Open
+from beancount.core.data import Commodity
 from beancount.core.number import Decimal
-from beancount.core.number import ZERO
 
 from fava.ext import FavaExtensionBase
 from fava.helpers import FavaAPIException
 from fava.template_filters import cost_or_value
 
-
 class InventoryList(FavaExtensionBase):  # pragma: no cover
-    """Sample Extension Report that just prints out an Inventory List."""
 
     report_title = "Inventory"
 
-    def portfolio_accounts(self):
-        """An account tree based on matching regex patterns."""
+    # returns types for query_table headers
+    def get_types(self):
+
+        types = []
+
+        types.append(("SKU", str(str)))
+        types.append(("Name", str(str)))
+
+        for account in self.config[0]:
+            types.append((account, str(Decimal)))
+
+        return types
+
+    # returns contents of query_table
+    def get_inventories(self):
+
         tree = self.ledger.root_tree
-        portfolios = []
+        commodities = self.ledger.all_entries_by_type[Commodity]
 
-        for option in self.config:
-            opt_key = option[0]
-            if opt_key == "account_name_pattern":
-                portfolio = self._account_name_pattern(tree, option[1])
-            elif opt_key == "account_open_metadata_pattern":
-                portfolio = self._account_metadata_pattern(
-                    tree, option[1][0], option[1][1]
-                )
-            else:
-                raise FavaAPIException("Portfolio List: Invalid option: " + opt_key)
-            portfolios.append(portfolio)
+        inventories = {}
 
-        return portfolios
+        for commodity in commodities:
+            if (self.config[1] is not None and re.compile("^.*" + self.config[1] +"$").match(commodity.meta['filename']) is not None):
+                inventory = {}
+                inventory['Name'] = commodity.meta['name']
+                inventories[commodity.currency] = inventory
 
-    def _account_name_pattern(self, tree, pattern):
-        """
-        Returns portfolio info based on matching account name.
-        Args:
-            tree: Ledger root tree node.
-            pattern: Account name regex pattern.
-        Return:
-            Data structured for use with a querytable (types, rows).
-        """
-        title = "Account names matching: '" + pattern + "'"
+        for inventory_account in self._get_inventory_accounts(tree):
+            label = inventory_account[0]
+            account = inventory_account[1]
+            for bal in account.balance:
+                sku = bal[0]
+                inventory = inventories.get(sku) if (inventories.get(sku) is not None) else {}
+                inventory[label] = account.balance[bal]
+                inventories[sku] = inventory
+
+        return self._get_rows_from_inventories(inventories)
+
+    # returns list of tuples (label, account_node)
+    def _get_inventory_accounts(self, tree):
         selected_accounts = []
-        regexer = re.compile(pattern)
-        for acct in tree.keys():
-            if (regexer.match(acct) is not None) and (
-                acct not in selected_accounts
-            ):
-                selected_accounts.append(acct)
+        for account in self.config[0]:
+            for key in tree.keys():
+                if (re.compile("^.*:" + account + "$").match(key) is not None):
+                    selected_accounts.append((account, tree[key]))
+                    break
+        
+        return selected_accounts
 
-        selected_nodes = [tree[x] for x in selected_accounts]
-        portfolio_data = self._portfolio_data(selected_nodes)
-        return title, portfolio_data
-
-    def _account_metadata_pattern(self, tree, metadata_key, pattern):
-        """
-        Returns portfolio info based on matching account open metadata.
-        Args:
-            tree: Ledger root tree node.
-            metadata_key: Metadata key to match for in account open.
-            pattern: Metadata value's regex pattern to match for.
-        Return:
-            Data structured for use with a querytable - (types, rows).
-        """
-        title = (
-            "Accounts with '"
-            + metadata_key
-            + "' metadata matching: '"
-            + pattern
-            + "'"
-        )
-        selected_accounts = []
-        regexer = re.compile(pattern)
-        for entry in self.ledger.all_entries_by_type[Open]:
-            if (metadata_key in entry.meta) and (
-                regexer.match(entry.meta[metadata_key]) is not None
-            ):
-                selected_accounts.append(entry.account)
-
-        selected_nodes = [tree[x] for x in selected_accounts]
-        portfolio_data = self._portfolio_data(selected_nodes)
-        return title, portfolio_data
-
-    def _portfolio_data(self, nodes):
-        """
-        Turn a portfolio of tree nodes into querytable-style data.
-        Args:
-            nodes: Account tree nodes.
-        Return:
-            types: Tuples of column names and types as strings.
-            rows: Dictionaries of row data by column names.
-        """
-        operating_currency = self.ledger.options["operating_currency"][0]
-        acct_type = ("account", str(str))
-        bal_type = ("balance", str(Decimal))
-        alloc_type = ("allocation", str(Decimal))
-        types = [acct_type, bal_type, alloc_type]
-
+    # returns rows as results based on inventories dictionary
+    def _get_rows_from_inventories(self, inventories):
         rows = []
-        portfolio_total = ZERO
-        for node in nodes:
+        for inventory in inventories:
             row = {}
-            row["account"] = node.name
-            balance = cost_or_value(node.balance)
-            if operating_currency in balance:
-                balance_dec = balance[operating_currency]
-                portfolio_total += balance_dec
-                row["balance"] = balance_dec
+            row["SKU"] = inventory
+
+            balances = inventories[inventory]
+            for balance in balances:
+                row[balance] = balances[balance]
+
             rows.append(row)
 
-        for row in rows:
-            if "balance" in row:
-                row["allocation"] = round(
-                    (row["balance"] / portfolio_total) * 100, 2
-                )
-
-        return types, rows
+        return rows
